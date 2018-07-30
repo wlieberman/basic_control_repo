fbaf4dba5ccae3fa6cf79282207392b7c7befdf7
Billy Lieberman <wlieberman@vmware.com>
2018-07-30 02:37:55 -0400

    Fixed some linting issues in spec files

--- spec/classes/profiles/base_spec.rb
+++ spec/classes/profiles/base_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe 'profiles::base' do
-  on_supported_os.each do |os,facts|
+  on_supported_os.each do |os, facts|
     context "with OS #{os}" do
-     let(:facts) do
+      let(:facts) do
         facts
       end
-     it { is_expected.to compile.with_all_deps }
+      it { is_expected.to compile.with_all_deps }
     end
   end
 end
